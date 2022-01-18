22a96c5824da1f1046001fe0c5d4dd562cc908d7
Alex Prokop <alex.prokop@gmail.com>
2022-01-18 13:56:18 -0500

    adds two flows

--- prefect/compare_bills.py
+++ prefect/compare_bills.py
@@ -15,21 +15,104 @@ from nltk.tokenize import RegexpTokenizer
 import prefect
 from prefect import task, Flow, Parameter
 from prefect.executors import LocalDaskExecutor
-
-import re
-
-import billsim
 from billsim import utils_db, pymodels
+from sqlalchemy.orm import sessionmaker
+from sqlmodel import create_engine
 
 
-def get_bills_to_compare()
-    A_doc_name = '117hr1319enr'
-    B_doc_name = '117hr1319eh'
+@task(log_stdout=True)
+def get_bills_to_compare():
 
-    return [[A_doc_name, B_doc_name]]
+    postgres_url = f"postgresql://postgres:postgres@localhost:5433"
+
+    engine = create_engine(postgres_url, echo=False)
+
+    SessionLocal = sessionmaker(autocommit=False,
+                                autoflush=False,
+                                expire_on_commit=False,
+                            bind=engine)
+                            
+    query = ("SELECT billtobill.bill_id, billtobill.bill_to_id, billtobill.score_es, " 
+        "bill1.billnumber AS bill1number, bill1.version AS bill1version, "
+        "bill2.billnumber as bill2number, bill2.version AS bill2version " 
+        "FROM billtobill "
+        "LEFT JOIN bill AS bill1 ON billtobill.bill_id = bill1.id "
+        "LEFT JOIN bill AS bill2 ON billtobill.bill_to_id = bill2.id "
+        "WHERE billtobill.score_es IS NOT NULL " 
+        "ORDER BY billtobill.bill_id ASC, billtobill.score_es DESC;")
+
+    rs = SessionLocal().execute(query)
+    results = rs.fetchall()
+
+    print("Number of records with value for ES score: ", len(results))
+    # group by bill_id
+    bill_data_obj = {}
+    for result in results:
+        score_es = result[2]
+        bill1numberversion = result[3] + result[4]
+        bill2numberversion = result[5] + result[6]
+
+        # TODO: add other docs to corpus and vector files
+        # if (bill1numberversion.startswith('116') or bill1numberversion.startswith('117')) and (bill2numberversion.startswith('116') or bill2numberversion.startswith('117')):
+        if not bill1numberversion in bill_data_obj.keys():
+            bill_data_obj[bill1numberversion] = []
+        # Format results like so: { '116hr5001ih' : [['116hr50002ih', 631.2436789999999], ['117hr333enr', 43.4343]] }
+        bill_data_obj[bill1numberversion].append([bill2numberversion, score_es])
+
+    # keep only the top 30 values for es_score for each bill_id  
+    new_bill_data_obj = {}
+
+    for key, value in bill_data_obj.items():
+        if len(value) < 30:
+            new_bill_data_obj[key] = value
+        else:
+            new_bill_data_obj[key] = value[0:30]
+    
+    count = 0
+    for val in new_bill_data_obj.values():
+        count = count + len(val)
 
 
-def calculate_bill_similarity(bill_pair):
+    print("Number of bills to compare: ", len(new_bill_data_obj.keys()))
+    print("Total number of comparisons to run: ", count)
+    
+    new_new_bill_data_obj = {}
+
+    # TODO: remove, only take 30 bills to compare for testing
+    bill_A_count = 0
+    bill_B_count = 0
+    for key, value in new_bill_data_obj.items():
+        if bill_A_count < 10:
+            new_new_bill_data_obj[key] = value
+            bill_A_count += 1
+            bill_B_count += len(value)
+
+    return new_new_bill_data_obj
+
+
+
+def document_tfidf_vectorized_transformation(document, doc_tfidf_vectorizer):
+    doc_vectorized = doc_tfidf_vectorizer.transform([document])
+    return doc_vectorized
+
+def section_doc_tfidf_vectorized_transformation(section_doc, sec_tfidf_vectorizer):
+    section_doc_vectorized = sec_tfidf_vectorizer.transform(section_doc)
+    return section_doc_vectorized
+
+# compute cosine pairwise similarity
+def cosine_pairwise_sim(a_vectorized, b_vectorized):
+    
+    #record time for computing similarity 
+    start = time.time()
+
+    sim_score =  cosine_similarity(a_vectorized, b_vectorized)
+
+    done = time.time()
+    elapsed = done - start
+    return elapsed, sim_score
+
+@task(log_stdout=True)
+def calculate_bill_similarity(bill_data_obj):
     doc_corpus_data = open("tv_doc_corpus_data.pickel", "rb")
     doc_tfidf_vectorizer = open("document_tfidf_vectorizer.pickel", "rb")
     section_corpus_data = open("tv_section_corpus_data.pickel", "rb")
@@ -40,54 +123,57 @@ def calculate_bill_similarity(bill_pair):
     doc_tfidf_vectorizer = pickle.load(doc_tfidf_vectorizer)
     sec_tfidf_vectorizer = pickle.load(sec_tfidf_vectorizer)
     
-    logger = prefect.context.get("logger")
+    start = time.time()
 
-    logger.info("Starting calc bill similarity.")
-    logger.warning("A warning message.")
-    print('Calculating bill similarity')
+    skip_count = 0
+    complete_count = 0
 
-    bill_a = bill_pair[0]
-    bill_b = bill_pair[1]
-    #pick any Document A & any Document B from data lists (at least that have more than 1 section)
-    A_doc_name = "BILLS-" + bill_a
-    B_doc_name = "BILLS-" + bill_b
- 
-    A_doc = [i[1] for i in doc_corpus_data if A_doc_name ==i[0]][0]
-    B_doc = [i[1] for i in doc_corpus_data if B_doc_name ==i[0]][0]
+    for bill_A, bills_to_compare_list in bill_data_obj.items():
+        for bill_info in bills_to_compare_list:  
+            bill_B = bill_info[0]
 
-    A_section_doc = [i[2] for i in section_corpus_data if A_doc_name ==i[0]]
-    B_section_doc = [i[2] for i in section_corpus_data if B_doc_name ==i[0]]
+            print("Running comparison: ", bill_A, " ", bill_B)
 
-    #transform document A content and document B content
-    A_doc_vectorized = document_tfidf_vectorized_transformation(A_doc, doc_tfidf_vectorizer)
-    B_doc_vectorized = document_tfidf_vectorized_transformation(B_doc, doc_tfidf_vectorizer)
-    #transform document A section content and  document B section content
-    A_section_doc_vectorized = section_doc_tfidf_vectorized_transformation(A_section_doc, sec_tfidf_vectorizer)
-    B_section_doc_vectorized = section_doc_tfidf_vectorized_transformation(B_section_doc, sec_tfidf_vectorizer)
+            #pick any Document A & any Document B from data lists (at least that have more than 1 section)
+            A_doc_name = "BILLS-" + bill_A
+            B_doc_name = "BILLS-" + bill_B
+        
+            try:
+                A_doc = [i[1] for i in doc_corpus_data if A_doc_name ==i[0]][0]
+                B_doc = [i[1] for i in doc_corpus_data if B_doc_name ==i[0]][0]
+            except IndexError:
+                print('Could not find both bills in corpus. Skipping.')
+                skip_count += 1
+                continue
 
-    elapsed_1, doc_sim_score = cosine_pairwise_sim(A_doc_vectorized, B_doc_vectorized)
-    elapsed_2, sec_doc_sim_score = cosine_pairwise_sim(A_section_doc_vectorized, B_section_doc_vectorized)
+            #transform document A content and document B content
+            A_doc_vectorized = document_tfidf_vectorized_transformation(A_doc, doc_tfidf_vectorizer)
+            B_doc_vectorized = document_tfidf_vectorized_transformation(B_doc, doc_tfidf_vectorizer)
 
-    # save to db
-    ids = utils_db.get_bill_ids(bill_pair)
+            elapsed_1, from_doc_similarity = cosine_pairwise_sim(A_doc_vectorized, B_doc_vectorized)
 
-    if len(ids) == 2 and ids[bill_a] != ids[bill_b]:
-        print("Both bills found. Saving.")
-        bill_id = ids[bill_a]
-        bill_to_id = ids[bill_b]
+            # save to db
+            ids = utils_db.get_bill_ids([bill_A, bill_B])
 
-        bill_to_bill_new = pymodels.BillToBillModel(
-            billnumber_version= bill_a,
-            billnumber_version_to= bill_b,
-            score= from_doc_similarity[0][0],
-            score_to= to_doc_similarity[0][0],
-            bill_id= bill_id,
-            bill_to_id= bill_to_id
-        )
+            if len(ids) == 2 and ids[bill_A] != ids[bill_B]:
+                print("Both bills found. Saving.")
+                print("Doc similarity: ", from_doc_similarity[0][0])
+                bill_id = ids[bill_A]
+                bill_to_id = ids[bill_B]
 
-        btb = utils_db.save_bill_to_bill(bill_to_bill_model= bill_to_bill_new)
+                # TODO: do we save 2 bill to bill records for each comparison?
+                bill_to_bill_new = pymodels.BillToBillModel(
+                    billnumber_version= bill_A,
+                    billnumber_version_to= bill_B,
+                    score= from_doc_similarity[0][0],
+                    bill_id= bill_id,
+                    bill_to_id= bill_to_id
+                )
 
-with Flow("Bill Level Comparisons", executor=LocalDaskExecutor()) as flow:
+                btb = utils_db.save_bill_to_bill(bill_to_bill_model= bill_to_bill_new)
+                complete_count += 1
+
+with Flow("Compare ES-related bills", executor=LocalDaskExecutor()) as flow:
     bills_to_compare = get_bills_to_compare()
     calculate_bill_similarity(bills_to_compare)
     
